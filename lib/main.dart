5b48aac200e6165e4242c0030e066d342262dfa7
phalkandari <mohammad.kandary@gmail.com>
2022-11-26 23:25:57 +0300

    Finished

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,11 @@ class MyApp extends StatelessWidget {
     "Biryani": "assets/biryani.jpg",
   };
 
+  // List<Food> foods = [
+  //   Food(name: "Pizza", image: "assets/pizza.jpg"),
+  //   Food(name: "Burger", image: "assets/burger.jpg")
+  // ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -28,47 +33,69 @@ class MyApp extends StatelessWidget {
           title: Text("Menu"),
         ),
         body: ListView(
-          children: [
-            Container(
-              margin: EdgeInsets.all(6),
+          children: menu.keys
+              .map((key) => FoodCard(name: key, url: menu[key]!))
+              .toList(),
+        ),
+      ),
+    );
+  }
+}
+
+class Food {
+  final String name;
+  final String image;
+
+  Food({
+    required this.name,
+    required this.image,
+  });
+}
+
+class FoodCard extends StatelessWidget {
+  final String name;
+  final String url;
+
+  FoodCard({required this.name, required this.url, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(6),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: Offset(0, 2),
+            ),
+          ]),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Text(
+            name,
+            style: TextStyle(fontSize: 26),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0), //container not image
+            child: Container(
+              clipBehavior: Clip.antiAlias,
               decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 2,
-                      blurRadius: 5,
-                      offset: Offset(0, 2),
-                    ),
-                  ]),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Text(
-                    "Pizza",
-                    style: TextStyle(fontSize: 26),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0), //container not image
-                    child: Container(
-                      clipBehavior: Clip.antiAlias,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      child: Image.asset(
-                        "pizza.jpg",
-                        height: 100,
-                        width: 100,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                ],
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Image.asset(
+                url,
+                height: 100,
+                width: 100,
+                fit: BoxFit.cover,
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
