ba074312173fe637b230c05e6b32b420b2d7114d
phalkandari <mohammad.kandary@gmail.com>
2022-11-26 23:09:41 +0300

    Stillllllllllll

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,75 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
-  // This widget is the root of your application.
+  final menu = {
+    "Pizza": "assets/pizza.jpg",
+    "Burger": "assets/burger.jpg",
+    "Pasta": "assets/pasta.jpg",
+    "Sushi": "assets/suchi.jpg",
+    "Biryani": "assets/biryani.jpg",
+  };
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: Scaffold(),
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Menu"),
+        ),
+        body: ListView(
+          children: [
+            Container(
+              margin: EdgeInsets.all(6),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: Offset(0, 2),
+                    ),
+                  ]),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text(
+                    "Pizza",
+                    style: TextStyle(fontSize: 26),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0), //container not image
+                    child: Container(
+                      clipBehavior: Clip.antiAlias,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: Image.asset(
+                        "pizza.jpg",
+                        height: 100,
+                        width: 100,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
